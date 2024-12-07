ecc24d5099041e0e55b85dc4bd9c749b96d6e10c
John Cavaliere <80801597+cavalierej@users.noreply.github.com>
2024-12-07 01:01:32 -0500

    Update 02. Analyze.sql

--- 02. Analyze.sql
+++ 02. Analyze.sql
@@ -2,10 +2,12 @@
 SELECT COUNT(*) AS trip_count
 FROM 'data-analytics-capstone-437301.capstone.yearly_data_cleaned'
 ;
+
 --Average trip length
 SELECT AVG(ride_length) AS average_trip_length
 FROM 'data-analytics-capstone-437301.capstone.yearly_data_cleaned'
 ;
+
 --Trip count by member type
 SELECT member_casual,
   COUNT(*) AS trip_count
@@ -13,6 +15,7 @@ FROM 'data-analytics-capstone-437301.capstone.yearly_data_cleaned'
 GROUP BY member_casual
 ORDER BY trip_count DESC
 ;
+
 --Trip count by rideable type
 SELECT rideable_type,
   COUNT(*) AS trip_count
@@ -20,6 +23,7 @@ FROM 'data-analytics-capstone-437301.capstone.yearly_data_cleaned'
 GROUP BY rideable_type
 ORDER BY trip_count DESC
 ;
+
 --Trip count by member type and rideable type
 SELECT member_casual,
   rideable_type,
@@ -29,18 +33,21 @@ GROUP BY member_casual,
   rideable_type
 ORDER BY trip_count DESC
 ;
+
 --Ride length by member type
 SELECT member_casual,
   AVG(ride_length_in_minutes) AS average_ride_length
 FROM 'data-analytics-capstone-437301.capstone.yearly_data_cleaned'
 GROUP BY member_casual
 ;
+
 --Ride length by bike type
 SELECT rideable_type,
   AVG(ride_length_in_minutes) AS average_ride_length
 FROM 'data-analytics-capstone-437301.capstone.yearly_data_cleaned'
 GROUP BY rideable_type
 ;
+
 --Ride length by member type and bike type
 SELECT member_casual
   rideable_type,
@@ -49,6 +56,7 @@ FROM 'data-analytics-capstone-437301.capstone.yearly_data_cleaned'
 GROUP BY member_casual,
   rideable_type
 ;
+
 --Trip count by hour
 SELECT EXTRACT(HOUR FROM started_at) AS hour,
   COUNT(*) AS trip_count
@@ -56,6 +64,7 @@ FROM 'data-analytics-capstone-437301.capstone.yearly_data_cleaned'
 GROUP BY hour
 ORDER BY trip_count DESC
 ;
+
 --Trip count by hour and member type
 SELECT EXTRACT(HOUR FROM started_at) AS hour,
   member_casual,
@@ -66,6 +75,7 @@ GROUP BY hour,
 ORDER BY hour,
   member_casual
 ;
+
 --Average ride length by hour
 SELECT EXTRACT(HOUR FROM started_at) AS hour,
   AVG(ride_length_in_minutes) AS average_ride_length
@@ -73,6 +83,7 @@ FROM 'data-analytics-capstone-437301.capstone.yearly_data_cleaned'
 GROUP BY hour
 ORDER BY hour
 ;
+
 --Average ride length by hour and member type
 SELECT EXTRACT(HOUR FROM started_at) AS hour,
   member_casual,
@@ -83,6 +94,7 @@ GROUP BY hour,
 ORDER BY hour,
   member_casual
 ;
+
 --Trip count by time of day
 SELECT time_of_day,
   COUNT(*) AS trip_count
@@ -90,6 +102,7 @@ FROM 'data-analytics-capstone-437301.capstone.yearly_data_cleaned'
 GROUP BY time_of_day
 ORDER BY trip_count DESC
 ;
+
 --Trip count by time of day and member type
 SELECT time_of_day,
   member_casual,
@@ -100,6 +113,7 @@ GROUP BY time_of_day,
 ORDER BY time_of_day,
   member_casual
 ;
+
 --Average ride length by time of day
 SELECT time_of_day,
   AVG(ride_length_in_minutes) AS average_ride_length
@@ -107,6 +121,7 @@ FROM 'data-analytics-capstone-437301.capstone.yearly_data_cleaned'
 GROUP BY time_of_day
 ORDER BY time_of_day
 ;
+
 --Average ride length by time of day and member type
 SELECT time_of_day,
   member_casual,
@@ -117,6 +132,7 @@ GROUP BY time_of_day,
 ORDER BY time_of_day,
   member_casual
 ;
+
 --Trip count by day
 SELECT day_of_week,
   COUNT(*) AS trip_count
@@ -124,6 +140,7 @@ FROM 'data-analytics-capstone-437301.capstone.yearly_data_cleaned'
 GROUP BY day_of_week
 ORDER BY trip_count DESC
 ;
+
 --Trip count by day and member type
 SELECT day_of_week,
   member_casual,
@@ -134,6 +151,7 @@ GROUP BY day_of_week,
 ORDER BY day_of_week,
   member_casual
 ;
+
 --Average ride length by day
 SELECT day_of_week,
   AVG(ride_length_in_minutes) AS average_ride_length
@@ -141,6 +159,7 @@ FROM 'data-analytics-capstone-437301.capstone.yearly_data_cleaned'
 GROUP BY day_of_week
 ORDER BY day_of_week
 ;
+
 --Average ride length by day and member type
 SELECT day_of_week,
   member_casual,
@@ -151,6 +170,7 @@ GROUP BY day_of_week,
 ORDER BY day_of_week,
   member_casual
 ;
+
 --Trip count by type of day
 SELECT type_of_day,
   COUNT(*) AS trip_count
@@ -158,6 +178,7 @@ FROM 'data-analytics-capstone-437301.capstone.yearly_data_cleaned'
 GROUP BY type_of_day
 ORDER BY trip_count DESC
 ;
+
 --Trip count by type of day and member type
 SELECT type_of_day,
   member_casual,
@@ -168,6 +189,7 @@ GROUP BY type_of_day,
 ORDER BY type_of_day,
   member_casual
 ;
+
 --Average ride length by type of day
 SELECT type_of_day,
   AVG(ride_length_in_minutes) AS average_ride_length
@@ -175,6 +197,7 @@ FROM 'data-analytics-capstone-437301.capstone.yearly_data_cleaned'
 GROUP BY type_of_day
 ORDER BY type_of_day
 ;
+
 --Average ride length by type of day and member type
 SELECT type_of_day,
   member_casual,
@@ -185,6 +208,7 @@ GROUP BY type_of_day,
 ORDER BY type_of_day,
   member_casual
 ;
+
 --Trip count by month
 SELECT EXTRACT(MONTH FROM started_at) AS month,
   COUNT(*) AS trip_count
@@ -192,6 +216,7 @@ FROM 'data-analytics-capstone-437301.capstone.yearly_data_cleaned'
 GROUP BY month
 ORDER BY trip_count DESC
 ;
+
 --Trip count by month and member type
 SELECT EXTRACT(MONTH FROM started_at) AS month,
   member_casual,
@@ -202,6 +227,7 @@ GROUP BY month,
 ORDER BY month,
   member_casual
 ;
+
 --Average ride length by month
 SELECT EXTRACT(MONTH FROM started_at) AS month,
   AVG(ride_length_in_minutes) AS average_ride_length
@@ -209,6 +235,7 @@ FROM 'data-analytics-capstone-437301.capstone.yearly_data_cleaned'
 GROUP BY month
 ORDER BY month
 ;
+
 --Average ride length by month and member type
 SELECT EXTRACT(HOUR FROM started_at) AS month,
   member_casual,
@@ -219,6 +246,7 @@ GROUP BY month,
 ORDER BY month,
   member_casual
 ;
+
 --Trip count by season
 SELECT season,
   COUNT(*) AS trip_count
@@ -226,6 +254,7 @@ FROM 'data-analytics-capstone-437301.capstone.yearly_data_cleaned'
 GROUP BY season
 ORDER BY trip_count DESC
 ;
+
 --Trip count by season and member type
 SELECT season,
   member_casual,
@@ -236,6 +265,7 @@ GROUP BY season,
 ORDER BY season,
   member_casual
 ;
+
 --Average ride length by season
 SELECT season,
   AVG(ride_length_in_minutes) AS average_ride_length
@@ -243,6 +273,7 @@ FROM 'data-analytics-capstone-437301.capstone.yearly_data_cleaned'
 GROUP BY season
 ORDER BY season
 ;
+
 --Average ride length by season and member type
 SELECT season,
   member_casual,
@@ -253,6 +284,7 @@ GROUP BY season,
 ORDER BY season,
   member_casual
 ;
+
 --Most frequent starting stations
 SELECT start_station_name,
   start_station_id,
@@ -263,6 +295,7 @@ GROUP BY start_station_name,
 HAVING trip_count >= 1000
 ORDER BY trip_count DESC
 ;
+
 --Most frequent ending stations
 SELECT end_station_name,
   end_station_id,
@@ -273,6 +306,7 @@ GROUP BY end_station_name,
 HAVING trip_count >= 1000
 ORDER BY trip_count DESC
 ;
+
 --Most frequent trips
 SELECT start_station_name,
   start_station_id,
